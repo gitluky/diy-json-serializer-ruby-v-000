59c90de4f081cb88ac79f809a095c7f0461aa308
Jonathan Luk <jluk87@gmail.com>
2019-08-31 19:07:40 +0000

    Automatically backed up by Learn

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -7,7 +7,11 @@ class PostSerializer
     serialized_post += '"id": ' + post.id.to_s + ', '
     serialized_post += '"title": "' + post.title + '", '
     serialized_post += '"description": "' + post.description + '", '
+    serialized_post += '"author": {'
+     serialized_post += '"name": "' + post.author.name + '"}'
+
+     # and end with the close brace
+     serialized_post += '}'
 
   end
-
 end
